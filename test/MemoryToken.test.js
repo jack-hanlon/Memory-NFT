1bc7ac0f614ce008177262d850ba53129863adb9
Jack Hanlon <jacklhanlon@yahoo.co.uk>
2021-08-18 22:30:02 -0700

    update

--- test/MemoryToken.test.js
+++ test/MemoryToken.test.js
@@ -29,7 +29,10 @@ contract('Memory Token', (accounts) => {
     })
   })
   describe('token distribution', async() => {
-    await token.mint(accounts[0]), 'https://www.token-uri.com/nft')
+    let result
+
+    it('mint tokens', async() => {
+    await token.mint(accounts[0], 'https://www.token-uri.com/nft')
     // It should increase the total supply
     result = await token.totalSupply()
     assert.equal(result.toString(),'1', 'total supply is correct')
@@ -44,6 +47,17 @@ contract('Memory Token', (accounts) => {
     // Owner can see all tokens
     let balanceOf = await token.balanceOf(accounts[0])
     let tokenIds = []
-    
+
+    for(let i = 0;i < balanceOf;i++){
+      let id = await token.tokenOfOwnerByIndex(accounts[0],i)
+      tokenIds.push(id.toString())
+    }
+    let expected = ['1']
+    assert.equal(tokenIds.toString(),expected.toString(),'tokenIds are correct')
+
+    // Token URI correct
+    let tokenURI = await token.tokenURI('1')
+    assert.equal(tokenURI,'https://www.token-uri.com/nft')
+    })
   })
 })
