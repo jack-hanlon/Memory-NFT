d29da0120168f20411e6dbe55ca42de6d4fecbb7
Jack Hanlon <jacklhanlon@yahoo.co.uk>
2021-08-18 21:26:57 -0700

    update

--- test/MemoryToken.test.js
+++ test/MemoryToken.test.js
@@ -5,5 +5,45 @@ require('chai')
   .should()
 
 contract('Memory Token', (accounts) => {
-  // code goes here...
+  let token
+
+  before(async() => {
+    token = await MemoryToken.deployed()
+  })
+  describe('deployment', async() => {
+    it('deploys successfully', async() => {
+      const address = token.address
+      assert.notEqual(address,0x0)
+      assert.notEqual(address,'')
+      assert.notEqual(address,null)
+      assert.notEqual(address,undefined)
+    })
+
+    it('has a name', async() => {
+      const name = await token.name()
+      assert.equal(name,'Memory Token')
+    })
+    it('has a symbol', async() => {
+      const symbol = await token.symbol()
+      assert.equal(symbol,'MEMORY')
+    })
+  })
+  describe('token distribution', async() => {
+    await token.mint(accounts[0]), 'https://www.token-uri.com/nft')
+    // It should increase the total supply
+    result = await token.totalSupply()
+    assert.equal(result.toString(),'1', 'total supply is correct')
+    // It increments owner balance
+    result = await token.balanceOf(accounts[0])
+    assert.equal(result.toString(),'1', 'balanceOf is correct')
+    // Token should belong to owner
+    result = await token.ownerOf('1')
+    assert.equal(result.toString(),accounts[0].toString(),'ownerOf is correct')
+    result = await token.tokenOfOwnerByIndex(accounts[0],0)
+
+    // Owner can see all tokens
+    let balanceOf = await token.balanceOf(accounts[0])
+    let tokenIds = []
+    
+  })
 })
